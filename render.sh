27d75704c9bbdde9c400f82cf964f6d0cd4e8919
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-05-27 21:41:08 +0000

    bumper

--- render.sh
+++ render.sh
@@ -2,5 +2,5 @@ ffmpeg -y \
 -i base.mp4 \
 -i title.mp4 \
 -filter_complex \
-"[1]format=rgb24,colorkey=black:0.1:1.0,colorchannelmixer=aa=1.0[1d]; [0][1d]overlay=0:0:enable='between(t, 0, 8)'[v1]" \
+"[1]format=rgb24,fade=in:st=3:d=1,colorkey=black:0.1:1.0,colorchannelmixer=aa=1.0[1d]; [0][1d]overlay=0:0:enable='between(t, 0, 8)',fade=out:st=7:d=1[v1]" \
 -map [v1] -c:a copy -c:v libx264 -preset ultrafast bumper.mp4
